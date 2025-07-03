8a6fab011a20c6a14d82573424c85ce752db18df
Jibin Joy <jibin.joy6713@gmail.com>
2025-07-03 12:38:51 +1200

    data_ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,10 +1,11 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_msg_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     error_message = f"Error occurred in script: [{exc_tb.tb_frame.f_code.co_filename}] at line number: [{exc_tb.tb_lineno}] error message: [{str(error)}]"
+    
     return error_message
 
     
@@ -17,4 +18,12 @@ class CustomException(Exception):
         
     def __str__(self):
         return self.error_message    
-    
+
+
+
+if __name__ == "__main__":    
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Divide by zero error")
+        raise CustomException(e,sys)
